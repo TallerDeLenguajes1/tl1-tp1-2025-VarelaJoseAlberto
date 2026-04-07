0b965f05729cd0034a1e30a32f2264cb8e0046f9
VarelajoseAlberto <varelaj298@gmail.com>
2026-04-07 14:11:04 -0300

    actualizacion del archivo tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // inciso 4a
     printf("hola mundo");
+
+    int variable = 10;
+    int *p_variable = &variable;
+
+    // 1) El contenido del puntero (la dirección a la que apunta) 
+    printf("1) Contenido del puntero: %p\n", (void*)p_variable);
+    
+    // 2) La direccion de memoria almacenada por el puntero 
+    // Nota: Debería ser igual al punto anterior.
+    printf("2) Direccion almacenada por el puntero: %p\n", (void*)p_variable);
+
+    // 3) La direccion de memoria de la variable original 
+    printf("3) Direccion de memoria de la variable: %p\n", (void*)&variable);
+
+    // 4) La direccion de memoria del propio puntero 
+    // ¡Ojo! Esta es la ubicación del puntero, no a donde apunta.
+    printf("4) Direccion de memoria del puntero: %p\n", (void*)&p_variable);
+
+    // 5) El tamaño de memoria utilizado por la variable 
+    printf("5) Tamaño de memoria de la variable: %zu bytes\n", sizeof(variable));
     return 0;
 }
