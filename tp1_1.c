a8ea6e2677428811b2a6a09f044e175159fa3ce3
VarelajoseAlberto <varelaj298@gmail.com>
2026-04-13 19:49:14 -0300

    punto 4 terminado

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,7 @@
 int main(int argc, char const *argv[])
 {
     // inciso 4a
-    printf("hola mundo");
+    printf("hola mundo\n");
 
     int variable = 10;
     int *p_variable = &variable;
@@ -11,14 +11,12 @@ int main(int argc, char const *argv[])
     printf("1) Contenido del puntero: %p\n", (void*)p_variable);
     
     // 2) La direccion de memoria almacenada por el puntero 
-    // Nota: Debería ser igual al punto anterior.
     printf("2) Direccion almacenada por el puntero: %p\n", (void*)p_variable);
 
     // 3) La direccion de memoria de la variable original 
     printf("3) Direccion de memoria de la variable: %p\n", (void*)&variable);
 
     // 4) La direccion de memoria del propio puntero 
-    // ¡Ojo! Esta es la ubicación del puntero, no a donde apunta.
     printf("4) Direccion de memoria del puntero: %p\n", (void*)&p_variable);
 
     // 5) El tamaño de memoria utilizado por la variable 
